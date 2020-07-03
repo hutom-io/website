391b5fe84123794f6456dabe4bd3e4166f211328
Geunho Jang <39117502+Rootjang92@users.noreply.github.com>
2020-07-03 21:09:19 +0900

    fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Route, Link } from 'react-router-dom';
+import { Route, Link, BrowserRouter } from 'react-router-dom';
 import './assets/scss/style.scss';
 import Home from './Home';
 import logo from './assets/images/logo.png'
@@ -7,7 +7,7 @@ import About from './About';
 
 const App = () => {
   return (
-    <HashRouter basename="/">
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
      <header className="header">
         <div className="wrap">
             <Link to="/"><h2><img src={logo} alt="hutom logo"/></h2></Link>
@@ -18,7 +18,7 @@ const App = () => {
       </header>
       <Route path="/" exact component={Home} />
       <Route path="/about" component={About} />
-    </HashRouter>
+    </BrowserRouter>
   )
 }
 
